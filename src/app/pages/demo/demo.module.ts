8a5e1cd1186a371acbeb9ab31ac05cceb4f55444
moneybharathe <dreamfuture4u@gmail.com>
2021-10-06 13:15:01 +0530

    5 files modified, 21 added

---
+++ src/app/pages/demo/demo.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import DemoRoutingModule from './demo.routing.module';
+import { DemoComponent } from './demo.component';
+
+@NgModule({
+  imports: [CommonModule, DemoRoutingModule],
+  declarations: [DemoComponent],
+})
+export class DemoModule {}
